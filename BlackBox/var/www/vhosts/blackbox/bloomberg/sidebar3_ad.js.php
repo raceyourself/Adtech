3c0a617beea6778cbcb0f3c9acf3d5afc01a6a4f
Janne Husberg <janne@raceyourself.com>
2014-08-15 17:06:02 +0100

    Prevent false positives: Run on readyState=='complete' instead of after a hard-coded timeout.

--- BlackBox/var/www/vhosts/blackbox/bloomberg/sidebar3_ad.js.php
+++ BlackBox/var/www/vhosts/blackbox/bloomberg/sidebar3_ad.js.php
@@ -1,24 +1,28 @@
 <?php require_once('obfuscator.php'); ?>
 <?php require_once('common.js.php'); ?>
 // Hide iframes that fail to load so we can detect the failures.
-// NOTE: This requires a long timeout and may only be necessary on IE
 var iframes = document.getElementsByTagName('iframe');
 for(var i=0, l=iframes.length; i < l; i++) {
   var iframe = iframes[i];
-  var iframeLoadTimeAllowed = 5000;
-  var timeout = setTimeout(function() {
-    iframe.style.setProperty('display', 'none');
-  }, iframeLoadTimeAllowed);
+
+  var hide = function() {
+    if(document.readyState === 'complete') iframe.style.setProperty('display', 'none');
+  };
+  document.addEventListener('readystatechange', hide);
 
   iframe.addEventListener('load', function() {
-      clearTimeout(timeout);
+    document.removeEventListener('readystatechange', hide);
   });
 }
 
-setTimeout(function() {
-  var divs = document.getElementsByClassName('google_adwords');
-  for (var i=0, l=divs.length; i < l; i++) {
-    if (!divs[i].id) divs[i].id = ~~(Math.random()*99999);
-    underad('.google_adwords#'+i, '300px', '250px', '<?php echo obfuscate('blackbox/ads/300x250_banner_add_placholder.png'); ?>', 'http://www.amazon.co.uk/Hawkins-Bazaar-Kitten-Walks-And-Meows/dp/B004DJ8O02/');
+document.addEventListener('readystatechange', function() {
+  if(document.readyState === 'complete') {
+    setTimeout(function() {
+      var divs = document.getElementsByClassName('google_adwords');
+      for (var i=0, l=divs.length; i < l; i++) {
+        if (!divs[i].id) divs[i].id = ~~(Math.random()*99999);
+        underad('.google_adwords#'+i, '300px', '250px', '<?php echo obfuscate('blackbox/ads/300x250_banner_add_placholder.png'); ?>', 'http://www.amazon.co.uk/Hawkins-Bazaar-Kitten-Walks-And-Meows/dp/B004DJ8O02/');
+      }
+    }, 100);
   }
-}, 5000);
+});
