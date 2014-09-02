2e3d3a7012cdbcd9b6009dd618326293823b7ec8
Duncan Roberts <dliroberts@gmail.com>
2014-09-02 14:47:11 +0100

    php obfuscation fixes

--- GameFAQs_v2/www.gamefaqs.com/obfuscator.php
+++ GameFAQs_v2/www.gamefaqs.com/obfuscator.php
@@ -4,29 +4,42 @@ require_once('sqaes.php');
 function obfuscate($path, $time) {
     $password = getPassword($time); # TODO
     $asset_path = '/assets/';
-    $time_bucket = 600;
+    $key_duration_secs = 600; # 10 mins
 
-    $period = intval($time / $time_bucket);
+    $period = intval($time / $key_duration_secs);
 
     # Timestamp as unencrypted param so that reverse proxy knows what key to use.
     return $asset_path . encrypt_with_password($period . $password, $path)  . "?$time";
 }
 
 function getPassword($time) {
-    $password;
-    for (row in rows) {
-        if (!row startswith(0-9))
-            continue;
+    $password = -1;
+    $file_lines = file('passwords.txt');
+    foreach ($file_lines as $file_index => $file_line) {
+    
+        if (!preg_match("/^[0-9]/", $file_line))
+            continue; # ignore 
         
-        rowTime = strtotime(row.split(",")[0]);
-        if ($time > rowTime) {
-            $password = row.substring(row.indexof(",") + 1);
+        $rowTime = strtotime(row.split(",")[0]);
+        if ($time > $rowTime) {
+            $password = after(',', $file_line);
         }
         else {
-            return $password;
+            break;
         }
     }
-    throw exception "no appropriate password"
+    if ($password === -1)
+        throw new Exception('No password currently in effect.');
+    return $password;
 }
 
+function startsWith($haystack, $needle) {
+    return $needle === "" || strpos($haystack, $needle) === 0;
+}
+
+function after($this, $inthat) {
+    if (!is_bool(strpos($inthat, $this)))
+        return substr($inthat, strpos($inthat,$this) + strlen($this));
+};
+    
 ?>
