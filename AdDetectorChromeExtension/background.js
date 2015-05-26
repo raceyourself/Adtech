adddcc2140976b74a7b56b10b3e8062795cf9e20
Duncan Roberts <dliroberts@gmail.com>
2015-05-26 12:10:56 +0100

    adblock background scripts no longer giving errors on load

--- AdDetectorChromeExtension/background.js
+++ AdDetectorChromeExtension/background.js
@@ -1,5 +1,26 @@
+// Setup/method dependencies required to keep AdBlock 'filtering/*.js' code happy.
+
 var _myfilters = new MyFilters();
 _myfilters.init();
+
+function get_custom_filters_text() { // dummy method, just to avoid code changes to 'filtering/*.js'.
+  return void 0;
+}
+
+function get_settings() { // defaults are fine for our purposes.
+  return {};
+}
+
+// Called when Chrome blocking needs to clear the in-memory cache.
+// No-op for Safari.
+function handlerBehaviorChanged() {
+  if (SAFARI)
+    return;
+  try {
+    chrome.webRequest.handlerBehaviorChanged();
+  } catch (ex) {
+  }
+}
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === 'identify_adverts') {
