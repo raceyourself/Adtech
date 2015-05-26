84bd87da6ba3c4db7dbe6c48dbd755edabea5d45
Duncan Roberts <dliroberts@gmail.com>
2015-05-26 17:16:18 +0100

    cleaned up folder naming

---
+++ AdDetectorChromeExtension/adblock_init.js
@@ -0,0 +1,28 @@
+// Setup/method dependencies required to keep AdBlock 'filtering/*.js' code happy.
+// Largely these are stubbed versions of definitions AdBlock has in its background.js.
+
+var _myfilters = new MyFilters();
+_myfilters.init();
+
+function get_custom_filters_text() { // dummy method, just to avoid code changes to 'filtering/*.js'.
+  return void 0;
+}
+
+function get_settings() { // defaults are fine for our purposes.
+  return {};
+}
+
+// Called when Chrome blocking needs to clear the in-memory cache.
+// No-op for Safari.
+function handlerBehaviorChanged() {
+  if (SAFARI)
+    return;
+  try {
+    chrome.webRequest.handlerBehaviorChanged();
+  } catch (ex) {
+  }
+}
+
+var STATS = {
+  version: '2.7.10'
+};
