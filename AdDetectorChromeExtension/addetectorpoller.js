d38c4ac2578a2b4039afa128f2c84aa954c029da
Duncan Roberts <dliroberts@gmail.com>
2015-06-02 19:37:02 +0100

    advert resource fetcher fixes

--- AdDetectorChromeExtension/addetectorpoller.js
+++ AdDetectorChromeExtension/addetectorpoller.js
@@ -6,6 +6,8 @@ var redis = require('redis');
 var util = require('util');
 var url = require('url');
 var http = require('http');
+var https = require('https');
+var mime = require('mime-types');
 
 var config = {};
 try {
@@ -24,95 +26,94 @@ config.queues = config.queues || {};
 
 var RES_PATH = './ad_resources/';
 
-var URLS_RETRIEVED_KEY = 'caress_advert_urls_retrieved';
-var NEXT_EVENT_KEY = 'caress_next_event_seq';
-var EVENTS_KEY = 'caress_advert_events';
-var PROCESSED_EVENTS_KEY = 'caress_advert_events_processed';
+var URLS_RETRIEVED_KEY   = config.queues.caress_advert_urls_retrieved   || 'caress_advert_urls_retrieved';
+var NEXT_EVENT_KEY       = config.queues.caress_next_event_seq          || 'caress_next_event_seq';
+//var EVENTS_KEY           = config.queues.caress_advert_events           || 'caress_advert_events';
+var EVENTS_KEY           = config.queues.caress_advert_events_copy           || 'caress_advert_events';
+var PROCESSED_EVENTS_KEY = config.queues.caress_advert_events_processed || 'caress_advert_events_processed';
 
-var FILENAME_EXTENSION_REGEX = /(.*)\.([^a-zA-Z0-9])+$/;
-var FILENAME_UNSAFE_FILENAME_REGEX = "[^a-zA-Z0-9.-]";
+var FILENAME_UNSAFE_FILENAME_REGEX = /[^a-zA-Z0-9.-]/g;
 
 var MAX_EVENTS_PER_POLL = 10;
 
+var MAX_FILENAME_LENGTH = 100;
+
 function nextJob() {
   redisClient.brpoplpush(EVENTS_KEY, PROCESSED_EVENTS_KEY, 0, function(error, event) {
-  
-  });
-  
-  redisClient.get(NEXT_EVENT_KEY, function(error, nextEventSeq) {
+  //redisClient.brpop(EVENTS_KEY, 0, function(error, event) {event = event[1];
     if (error) {
-      console.log('Failed to fetch caress_next_event_seq: ' + error);
+      console.log('Failed to pop element from ' + EVENTS_KEY + 'and move to ' + PROCESSED_EVENTS_KEY);
       return;
     }
-    redisClient.llen(EVENTS_KEY, function(error, availableEvents) {
-      if (error) {
-        console.log('Failed to count rows in caress_advert_events: ' + error);
-        return;
-      }
-      var eventsRemaining = availableEvents - nextEventSeq;
-      var toFetch = eventsRemaining > MAX_EVENTS_PER_POLL ? MAX_EVENTS_PER_POLL : eventsRemaining;
-      var fetchEnd = lastEventSeq + toFetch;
+    processEvent(event);
+  });
+}
 
-      redisClient.lrange(caress_advert_events, nextEventSeq, fetchEnd, function(error, results) {
+function processEvent(eventStr) {
+  var event = JSON.parse(eventStr);
+  var source = event.source;
+  
+  redisClient.hexists(URLS_RETRIEVED_KEY, source, function(error, exists) {
+    if (exists) {
+      console.log('Source already fetched: ' + source);
+    }
+    else {
+      fetchResource(source);
+    }
+  });
+}
+
+function fetchResource(source) {
+  var parsedUrl = url.parse(source);
+  var urlPath = parsedUrl.path;
+
+  var requestOptions = {};
+  requestOptions.host = parsedUrl.host;
+  requestOptions.port = parsedUrl.port;
+  requestOptions.path = parsedUrl.path;
+  if (parsedUrl.search) {
+    requestOptions.path += parsedUrl.search;
+  }
+  
+  console.log('Sending request: ' + JSON.stringify(requestOptions));
+  
+  var prot = parsedUrl.protocol === 'https:' ? https : http;
+  
+  prot.get(requestOptions, function onResourceDownloaded(response) {
+    var contentType = response.headers['content-type'];
+    console.log('Got response. Content-Type=' + contentType);
+
+    if (urlPath.length > MAX_FILENAME_LENGTH)
+      urlPath.substring(0, MAX_FILENAME_LENGTH);
+    var sanitisedUniqueFilename = new Date().getTime() + '_' + urlPath.replace(FILENAME_UNSAFE_FILENAME_REGEX, '_') + '.' + mime.extension(contentType);
+
+    var sanitisedUniquePath = path.join(RES_PATH, sanitisedUniqueFilename);
+
+    response.setEncoding('binary'); // this
+
+    var resourceBinary = '';
+    response.on('error', function(err) {
+      console.log("Error during HTTP request");
+      console.log(err.message);
+    });
+
+    response.on('data', function(chunk) {
+        return resourceBinary += chunk;
+    });
+    response.on('end', function() {
+      fs.writeFile(sanitisedUniquePath, resourceBinary, 'binary', function(error) {
         if (error) {
-          console.log('Failed to fetch rows from caress_advert_events: ' + error);
+          console.log('Unable to write resource to ' + sanitisedUniquePath + ': ' + error);
           return;
         }
-        results.forEach(function(eventStr) {
-          var event = JSON.parse(eventStr);
-          var source = event.source;
-          
-          redisClient.hexists(URLS_RETRIEVED_KEY, source, function(error, exists) {
-            if (exists) {
-              console.log('Source already fetched: ' + source);
-            }
-            else {
-              var parsedUrl = url.parse(source);
-              var path = parsedUrl.path;
-              
-              var filenameExtnMatch = FILENAME_EXTENSION_REGEX.exec(path);
-              
-              var sanitisedUniqueFilename = new Date().getTime() + '_' + (filenameExtnMatch ?
-                (filenameExtnMatch[1].replaceAll(FILENAME_UNSAFE_FILENAME_REGEX, '_') + '.' + filenameExtnMatch[2]) :
-                path.replaceAll(FILENAME_UNSAFE_FILENAME_REGEX, '_'));
-              
-              var sanitisedUniquePath = path.join(RES_PATH, sanitisedUniqueFilename);
-              
-              var requestOptions = parsedUrl;
-              requestOptions.method = 'GET';
-              requestOptions.encoding = null;
+        console.log('Wrote file ' + sanitisedUniquePath);
+        
+        redisClient.hsetnx(URLS_RETRIEVED_KEY, url, sanitisedUniquePath);
 
-              http.request(requestOptions, function(res) {
-                res.setEncoding('binary'); // this
-
-                var resourceBinary = '';
-                res.on('error', function(err) {
-                    console.log("Error during HTTP request");
-                    console.log(err.message);
-                });
-                
-                res.on('data', function(chunk) {
-                    return resourceBinary += chunk;
-                });
-                res.on('end', function() {
-                  fs.writeFile(sanitisedUniquePath, resourceBinary, 'binary', function(error) {
-                    if (error) {
-                      console.log('Unable to write resource to ' + sanitisedUniquePath + ': ' + error);
-                      return;
-                    }
-                    redisClient.hsetnx(URLS_RETRIEVED_KEY, url, sanitisedUniquePath);
-                    
-                    process.nextTick(nextJob);
-                  });
-                });
-              }
-            }
-          });
-        });
-
-        redisClient.set(NEXT_EVENT_KEY, fetchEnd + 1);
+        process.nextTick(nextJob);
       });
     });
   });
 }
+
 nextJob();
